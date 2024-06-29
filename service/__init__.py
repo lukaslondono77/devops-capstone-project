8709eef78a74d0e3db66959c4282852bf0818f7f
Lukas Londono <152759905+lukaslondono77@users.noreply.github.com>
2024-06-29 14:48:09 -0500

    Update __init__.py

--- service/__init__.py
+++ service/__init__.py
@@ -1,19 +1,28 @@
+"""
+Package: service
+Package for the application models and service routes
+This module creates and configures the Flask app and sets up the logging
+and SQL database
+"""
 import sys
 from flask import Flask
 from service import config
 from service.common import log_handlers
-from flask_talisman import Talisman  # Import Talisman
-from flask_cors import CORS  # Import CORS
+from flask_talisman import Talisman
+from flask_cors import CORS
 
 # Create Flask application
 app = Flask(__name__)
+talisman = Talisman(app)
+CORS(app)
 app.config.from_object(config)
 
 # Import the routes After the Flask app is created
-from service import routes, models
+# pylint: disable=wrong-import-position, cyclic-import, wrong-import-order
+from service import routes, models  # noqa: F401 E402
 
-# Import error handlers and CLI commands
-from service.common import error_handlers, cli_commands
+# pylint: disable=wrong-import-position
+from service.common import error_handlers, cli_commands  # noqa: F401 E402
 
 # Set up logging for production
 log_handlers.init_logging(app, "gunicorn.error")
@@ -24,14 +33,9 @@ app.logger.info(70 * "*")
 
 try:
     models.init_db(app)  # make our database tables
-except Exception as error:
+except Exception as error:  # pylint: disable=broad-except
     app.logger.critical("%s: Cannot continue", error)
+    # gunicorn requires exit code 4 to stop spawning workers when they die
     sys.exit(4)
-
-# Initialize Talisman with the Flask app
-talisman = Talisman(app)
-
-# Initialize CORS with the Flask app
-CORS(app)
 
 app.logger.info("Service initialized!")
