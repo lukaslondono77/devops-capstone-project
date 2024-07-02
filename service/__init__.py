7cba772effc7bcb23bea5050f162676af4fb0982
Lukas Londono <152759905+lukaslondono77@users.noreply.github.com>
2024-07-02 12:17:09 -0500

    Update __init__.py

--- service/__init__.py
+++ service/__init__.py
@@ -19,12 +19,13 @@ app.config.from_object(config)
 talisman = Talisman(app)
 CORS(app)
 
-# Import the routes After the Flask app is created
+# Import the routes and models after the Flask app is created
 # pylint: disable=wrong-import-position, cyclic-import, wrong-import-order
-import service.routes  # noqa: F401 E402
-import service.models  # noqa: F401 E402
-import service.common.error_handlers  # noqa: F401 E402
-import service.common.cli_commands  # noqa: F401 E402
+from service import routes, models  # noqa: F401 E402
+
+# Import error handlers and CLI commands
+# pylint: disable=wrong-import-position
+from service.common import error_handlers, cli_commands  # noqa: F401 E402
 
 # Set up logging for production
 log_handlers.init_logging(app, "gunicorn.error")
@@ -34,7 +35,7 @@ app.logger.info("  A C C O U N T   S E R V I C E   R U N N I N G  ".center(70, "
 app.logger.info(70 * "*")
 
 try:
-    service.models.init_db(app)  # make our database tables
+    models.init_db(app)  # make our database tables
 except Exception as error:  # pylint: disable=broad-except
     app.logger.critical("%s: Cannot continue", error)
     # gunicorn requires exit code 4 to stop spawning workers when they die
