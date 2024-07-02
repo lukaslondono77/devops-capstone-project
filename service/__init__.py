132ddb40a0ba668292fe6428c9d68906dba9191f
Lukas Londono <152759905+lukaslondono77@users.noreply.github.com>
2024-07-02 12:08:21 -0500

    Update __init__.py

--- service/__init__.py
+++ service/__init__.py
@@ -15,16 +15,16 @@ from flask_cors import CORS
 app = Flask(__name__)
 app.config.from_object(config)
 
-# Import the routes After the Flask app is created
-# pylint: disable=wrong-import-position, cyclic-import, wrong-import-order
-from service import routes, models  # noqa: F401 E402
-
-# pylint: disable=wrong-import-position
-from service.common import error_handlers, cli_commands  # noqa: F401 E402
-
 # Configure Talisman for security headers
 talisman = Talisman(app)
 CORS(app)
+
+# Import the routes After the Flask app is created
+# pylint: disable=wrong-import-position, cyclic-import, wrong-import-order
+import service.routes  # noqa: F401 E402
+import service.models  # noqa: F401 E402
+import service.common.error_handlers  # noqa: F401 E402
+import service.common.cli_commands  # noqa: F401 E402
 
 # Set up logging for production
 log_handlers.init_logging(app, "gunicorn.error")
@@ -34,7 +34,7 @@ app.logger.info("  A C C O U N T   S E R V I C E   R U N N I N G  ".center(70, "
 app.logger.info(70 * "*")
 
 try:
-    models.init_db(app)  # make our database tables
+    service.models.init_db(app)  # make our database tables
 except Exception as error:  # pylint: disable=broad-except
     app.logger.critical("%s: Cannot continue", error)
     # gunicorn requires exit code 4 to stop spawning workers when they die
