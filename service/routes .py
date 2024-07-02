2a3bd1adfd5c0c4225595cdd10d883ef52b40896
Lukas Londono <152759905+lukaslondono77@users.noreply.github.com>
2024-07-02 12:08:56 -0500

    Update routes .py

--- service/routes .py
+++ service/routes .py
@@ -7,7 +7,7 @@ This microservice handles the lifecycle of Accounts
 from flask import jsonify, request, make_response, abort, url_for   # noqa: F401
 from service.models import Account
 from service.common import status  # HTTP Status Codes
-from . import app  # Import Flask application
+from service import app  # Import Flask application
 
 BASE_URL = "/accounts"
 
@@ -105,43 +105,4 @@ def update_accounts(account_id):
     """
     app.logger.info("Request to update an Account with id: %s", account_id)
 
-    account = Account.find(account_id)
-    if not account:
-        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-
-    account.deserialize(request.get_json())
-    account.update()
-
-    return jsonify(account.serialize()), status.HTTP_200_OK
-
-
-######################################################################
-# DELETE AN ACCOUNT
-######################################################################
-@app.route("/accounts/<int:account_id>", methods=["DELETE"])
-def delete_accounts(account_id):
-    """
-    Delete an Account
-    This endpoint will delete an Account based on the account_id that is requested
-    """
-    app.logger.info("Request to delete an Account with id: %s", account_id)
-    account = Account.find(account_id)
-    if not account:
-        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-    account.delete()
-    return "", status.HTTP_204_NO_CONTENT
-
-
-######################################################################
-#  U T I L I T Y   F U N C T I O N S
-######################################################################
-def check_content_type(media_type):
-    """Checks that the media type is correct"""
-    content_type = request.headers.get("Content-Type")
-    if content_type and content_type == media_type:
-        return
-    app.logger.error("Invalid Content-Type: %s", content_type)
-    abort(
-        status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
-        f"Content-Type must be {media_type}",
-    )
+    account =
